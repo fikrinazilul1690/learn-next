d65506fbfb07f6d6964ea5dcaa256836e2b74886
baihaqilp <fikrinazilul@gmail.com>
2022-09-20 11:15:58 +0700

    API Post request

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -7,5 +7,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).send(comments);
+  if (req.method === 'GET') {
+    res.status(200).send(comments);
+  } else if (req.method === 'POST') {
+    const comment = req.body.comment;
+
+    const newComment = {
+      id: comments.length + 1,
+      text: comment,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  }
 }
